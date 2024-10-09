805d10ea022c3dfe51d3906bcf347f5b773a1a97
Hasrat Parwez <91671497+hasratparwez9162@users.noreply.github.com>
2024-10-09 14:56:09 +0530

    first commit

---
+++ src/main/java/com/bank/app/consumer/CardEventConsumer.java
@@ -0,0 +1,18 @@
+package com.bank.app.consumer;
+
+//import com.bank.core.entity.CardNotification;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class CardEventConsumer {
+
+
+//    @KafkaListener(topics = "card-service-topic",groupId = "bank-app")
+//    public void consumeEvents(ConsumerRecord<String, CardNotification> record) {
+//        System.out.println(record.key() +": "+ record.value().toString());
+//    }
+}
+
---
+++ src/main/java/com/bank/app/consumer/LoanEventConsumer.java
@@ -0,0 +1,22 @@
+package com.bank.app.consumer;
+
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoanEventConsumer {
+    @Autowired
+    private ObjectMapper objectMapper;
+
+//    @KafkaListener(topics = "loan-service-topic", groupId = "bank-app")
+//    public void consumeLoanEvent( ConsumerRecord<String, LoanNotification> record) {
+//        String eventType = record.key();
+//        LoanNotification loanJson = record.value();
+//        System.out.println(eventType +" : " + loanJson.toString());
+//    }
+
+}
